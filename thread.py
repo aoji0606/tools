c9697aa7547de8318c576dda41f39063f67b5f6c
unknown <aoji@360.cn>
2024-05-30 10:36:52 +0800

    a

--- thread.py
+++ thread.py
@@ -1,10 +1,14 @@
 from threading import Thread
+import traceback
 
 
 def run(thread_id, data):
-    for i in data:
-        # do ***
-        print(thread_id, i)
+    for item in data:
+        try:
+            # do ***
+            print(thread_id, item)
+        except:
+            traceback.print_exc()
     
 
 data = list(range(100))
@@ -17,6 +21,7 @@ for i in range(thread_num):
     end_index = start_index + chunk_size if i < thread_num - 1 else data_size
     chunk = data[start_index:end_index]
     threads.append(Thread(target=run, args=(i, chunk)))
+    print(f"thread:{i} size:{len(chunk)}")
 for i in threads:
     i.start()
 for i in threads:
