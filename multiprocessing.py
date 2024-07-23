7e40ac3023f0d4167f394728c6449df82810f6fc
unknown <aoji@360.cn>
2024-07-23 15:38:52 +0800

    update lock

--- multiprocessing.py
+++ multiprocessing.py
@@ -1,12 +1,15 @@
-import multiprocessing
 import traceback
+import multiprocessing
+from tqdm import tqdm
 
 
-def run(thread_id, data):
-    for item in data:
+def run(thread_id, data, lock):
+    for item in tqdm(data):
         try:
             # do ***
+            lock.acquire()
             print(thread_id, item)
+            lock.release()
         except:
             traceback.print_exc()
     
@@ -16,11 +19,12 @@ data_size = len(data)
 thread_num = 10
 chunk_size = data_size // thread_num
 processes = []
+lock = multiprocessing.Lock()
 for i in range(thread_num):
     start_index = i * chunk_size
     end_index = start_index + chunk_size if i < thread_num - 1 else data_size
     chunk = data[start_index:end_index]
-    processes.append(multiprocessing.Process(target=run, args=(i, chunk)))
+    processes.append(multiprocessing.Process(target=run, args=(i, chunk, lock)))
     print(f"processes:{i} size:{len(chunk)}")
 for i in processes:
     i.start()
